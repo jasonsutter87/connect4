4a86f14073c2a96cf26d075d1ae919b397cf5d7a
Jason Sutter <jasonsutter87@gmail.com>
2016-09-23 10:40:55 -0700

    Added Spec runner, started JS for game.

---
+++ js/application.js
@@ -0,0 +1,10 @@
+$(document).on('ready', function(){
+	console.log("READY")
+})
+
+
+//document load
+//init 
+//start game
+//update board
+//key binding events
---
+++ js/game.js
@@ -0,0 +1,32 @@
+var Game = function(boardString) {
+  this.boardMatrix = [];
+  this.tempBoard = [];
+  this.board = ['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''];
+ }
+
+
+Game.prototype.boardToMatrix = = function(){}
+Game.prototype.matrixToBoard = = function(){}
+Game.prototype.whoesTurn = = function(){}
+Game.prototype.placeDisc = = function(){}
+
+
+Game.prototype.transpose = function() {
+  var array = this.boardMatrix;
+  this.boardMatrix = array[0].map(function(col, i) {
+    return array.map(function(row) {
+        return row[i]
+    })
+  });
+ }
+
+Game.prototype.whoWon = = function(){}
+Game.prototype.gameOver = = function(){}
+Game.prototype.checkRows = = function(){}
+Game.prototype.checkDiangles = = function(){}
+
+
+
+//Driver Code
+game = new Game();
+console.log(game.board);
