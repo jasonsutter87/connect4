d45ac09d9578dd16623a9e3605dd9b9af5d1cd76
RenanBa <renanbarbosadesouza@gmail.com>
2016-09-26 18:22:39 -0700

    Refactored

--- js/application.js
+++ js/application.js
@@ -8,7 +8,6 @@ $(document).on('ready', function(){
 //start game
 //update board
 
-//function to reverse the html object
 reverse = function(array){
   var reversedArray = [];
   for (column = 1; column < 7; column++){ reversedArray.push(array.eq(-column)); }
@@ -16,31 +15,15 @@ reverse = function(array){
 }
 
 clickEvents = function(){
-
-  //This function will know where the user has clicked and will be able to display all element that belongs to that column
   $(".center .box").on("click", function(e){
-    //select the name of the class that was clicked. It will be a string with three class names example: ["boxTan box col1"]
-    var col = $(this).attr("class");
-    //Slice the class name to get all elements that belongs to that column. The class name we need is "col#"
-    var colArray = $("."+col.slice(11));
-    //Reverse the array
-    var reversed = reverse(colArray);
-
-    //this done is just to stop the if. Also saving whoseTurn before get into the loop
+    var reversed = reverse($("."+$(this).attr("class").slice(11)));
     var done = false;
     var whoseTurn = game.whoseTurn();
-
     $.each(reversed, function( index, value ) {
-
-      //if there are spot available replace class with the right color
       if(value.hasClass("boxTan") && done == false){
-        if(whoseTurn == "R"){
-          value.removeClass("boxTan").addClass("boxRed");
-          done = true;
-        }else if(whoseTurn == "B"){
-          value.removeClass("boxTan").addClass("boxBlack");
-          done = true;
-        }
+        if(whoseTurn == "R"){value.removeClass("boxTan").addClass("boxRed");}
+        else if(whoseTurn == "B"){value.removeClass("boxTan").addClass("boxBlack");}
+        done = true;
       }
     });
   })
